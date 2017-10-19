4c6befcbf73715611d6c3d2a296605a7d8b8ac41
TJ DeVries <devries.timothyj@gmail.com>
2017-10-19 07:38:45 -0500

    Add holding F for alt

--- keyboards/ergodox/keymaps/tj/keymap.c
+++ keyboards/ergodox/keymaps/tj/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Del    |   Q  | W/L3 |   E  |   R  |   T  |Insrt |           | LDR  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Grv    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  | J/Alt|   K  |   L  |; / L2|   '    |
+ * | Grv    |   A  |   S  |   D  | F/Alt|   G  |------|           |------|   H  | J/Alt|   K  |   L  |; / L2|   '    |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift | Ctrl |x:M_CS|   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| ~L1    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,         KC_2,           KC_3,           KC_4,            KC_5,   KC_LEFT,
         KC_DELT,        KC_Q,         LT(WORK, KC_W), KC_E,           KC_R,            KC_T,   KC_INSERT,
-        KC_GRV,         KC_A,         KC_S,           KC_D,           KC_F,            KC_G,
+        KC_GRV,         KC_A,         KC_S,           KC_D,           ALT_T(KC_F),     KC_G,
         KC_LSFT,        KC_LCTL,      MT(M_CS, KC_X), KC_C,           KC_V,            KC_B,   ALL_T(KC_NO),
         KC_Z,           KC_QUOT,      LALT(KC_LSFT),  KC_LEFT, KC_RGHT,
                                                      KC_FN2,   KC_LGUI,
